f35fea10198b17156c1ee2d8dbb45c8ac2044c0c
hanwei <hanwei2008123@163.com>
2023-04-05 19:29:10 +0800

    multi pages

---
+++ pages/1_📈_Plotting_Demo.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+############################
+#File Name: 1_📈_Plotting_Demo.py
+#Author: 韩伟
+#Mail: hanwei2008123@163.com
+#Created Time: 2023-04-05 19:14:44
+############################
+
+import streamlit as st
+import time
+import numpy as np
+
+st.set_page_config(page_title="Plotting Demo", page_icon="📈")
+
+st.markdown("# Plotting Demo")
+st.sidebar.header("Plotting Demo")
+st.write(
+    """This demo illustrates a combination of plotting and animation with
+Streamlit. We're generating a bunch of random numbers in a loop for around
+5 seconds. Enjoy!"""
+)
+
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
+last_rows = np.random.randn(1, 1)
+chart = st.line_chart(last_rows)
+
+for i in range(1, 101):
+    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+    status_text.text("%i%% Complete" % i)
+    chart.add_rows(new_rows)
+    progress_bar.progress(i)
+    last_rows = new_rows
+    time.sleep(0.05)
+
+progress_bar.empty()
+
+# Streamlit widgets automatically run the script from top to bottom. Since
+# this button is not connected to any other logic, it just causes a plain
+# rerun.
+st.button("Re-run")
